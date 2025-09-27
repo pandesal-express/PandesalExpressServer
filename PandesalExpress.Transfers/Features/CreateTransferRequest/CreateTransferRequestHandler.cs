90ea4103c0a63bfadeb009f10baa78f3ca98cd58
faustojc <faustojcboko@gmail.com>
2025-09-27 16:17:19 +0800

    Caching some transfer request data for real-time

--- PandesalExpress.Transfers/Features/CreateTransferRequest/CreateTransferRequestHandler.cs
+++ PandesalExpress.Transfers/Features/CreateTransferRequest/CreateTransferRequestHandler.cs
@@ -1,16 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using PandesalExpress.Infrastructure.Abstractions;
 using PandesalExpress.Infrastructure.Context;
 using PandesalExpress.Infrastructure.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
+using PandesalExpress.Infrastructure.Services;
 using Shared.Dtos;
 using Shared.Events;
+using StackExchange.Redis;
 
 namespace PandesalExpress.Transfers.Features.CreateTransferRequest;
 
 public class CreateTransferRequestHandler(
     AppDbContext context,
-    IEventBus eventBus
+    IEventBus eventBus,
+    ICacheService cacheService
 ) : ICommandHandler<CreateTransferRequestCommand, TransferRequestDto>
 {
     public async Task<TransferRequestDto> Handle(CreateTransferRequestCommand request, CancellationToken cancellationToken)
@@ -22,14 +25,17 @@ public class CreateTransferRequestHandler(
             InitiatingEmployeeId = request.InitiatingEmployeeId,
             RequestNotes = request.CreateTransferRequestDto.RequestNotes,
             Status = TransferStatus.Requested,
-            Items = [.. request.CreateTransferRequestDto.Items.Select(i => new TransferRequestItem
-                {
-                    ProductId = i.ProductId,
-                    ProductName = i.ProductName,
-                    QuantityRequested = i.Quantity
-                }
-            )]
-		};
+            Items =
+            [
+                .. request.CreateTransferRequestDto.Items.Select(i => new TransferRequestItem
+                    {
+                        ProductId = i.ProductId,
+                        ProductName = i.ProductName,
+                        QuantityRequested = i.Quantity
+                    }
+                )
+            ]
+        };
 
         var transferRequestDto = new TransferRequestDto
         {
@@ -39,15 +45,18 @@ public class CreateTransferRequestHandler(
             InitiatingEmployeeId = transferRequest.InitiatingEmployeeId.ToString(),
             Status = transferRequest.Status.ToString(),
             RequestNotes = transferRequest.RequestNotes,
-            Items = [.. transferRequest.Items.Select(i => new TransferRequestItemDto
-                {
-                    Id = i.Id.ToString(),
-                    ProductId = i.ProductId.ToString(),
-                    ProductName = i.ProductName,
-                    QuantityRequested = i.QuantityRequested
-                }
-            )]
-		};
+            Items =
+            [
+                .. transferRequest.Items.Select(i => new TransferRequestItemDto
+                    {
+                        Id = i.Id.ToString(),
+                        ProductId = i.ProductId.ToString(),
+                        ProductName = i.ProductName,
+                        QuantityRequested = i.QuantityRequested
+                    }
+                )
+            ]
+        };
 
         await using IDbContextTransaction transaction = await context.Database.BeginTransactionAsync(cancellationToken);
         try
@@ -56,6 +65,28 @@ public class CreateTransferRequestHandler(
             await context.SaveChangesAsync(cancellationToken);
             await transaction.CommitAsync(cancellationToken);
 
+            // Cache the transfer request
+            var cacheKey = $"transfer:{transferRequest.Id}";
+
+            // Cache the status, request notes, and response notes only as json values
+            HashEntry[] cacheValues =
+            [
+                new("status", transferRequest.Status.ToString()),
+                new("sending_store_id", transferRequest.SendingStoreId.ToString()),
+                new("receiving_store_id", transferRequest.ReceivingStoreId.ToString())
+            ];
+
+            if (!string.IsNullOrEmpty(transferRequest.RequestNotes))
+                cacheValues = cacheValues
+                              .Append(new HashEntry("request_notes", transferRequest.RequestNotes))
+                              .ToArray();
+
+            if (!string.IsNullOrEmpty(transferRequest.ResponseNotes))
+                cacheValues = cacheValues
+                              .Append(new HashEntry("response_notes", transferRequest.ResponseNotes))
+                              .ToArray();
+
+            await cacheService.SetHashAsync(cacheKey, cacheValues, TimeSpan.FromHours(2));
             await eventBus.PublishAsync(new TransferRequestCreatedEvent(transferRequestDto), cancellationToken);
         }
         catch (DbUpdateConcurrencyException e)
